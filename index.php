cc8c61cf561996cf432fb8a3d84b21e94aa84704
Riley Drake <rileydrakedesign@gmail.com>
2023-09-13 21:48:22 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Main theme file
+ * 
+ * @package drakeDigitalTheme
+ * 
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>drakeDigitalTheme</title>
+</head>
+<body>
+    
+</body>
+</html>
